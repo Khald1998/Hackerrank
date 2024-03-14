5d9bdf628ce25c1b70dbe9e7bc68f6e5d291ba7b
Khald1998 <108824412+Khald1998@users.noreply.github.com>
2024-03-14 20:16:39 +0300

    -

---
+++ Number Line Jumps/JavaScript/main.js
@@ -0,0 +1,70 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', function(inputStdin) {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', function() {
+    inputString = inputString.split('\n');
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/*
+ * Complete the 'kangaroo' function below.
+ *
+ * The function is expected to return a STRING.
+ * The function accepts following parameters:
+ *  1. INTEGER x1
+ *  2. INTEGER v1
+ *  3. INTEGER x2
+ *  4. INTEGER v2
+ */
+
+function kangaroo(x1, v1, x2, v2) {
+    // If kangaroos jump at the same rate but are not at the same position, they will never meet.
+    if (v1 === v2) {
+        return x1 === x2 ? "YES" : "NO";
+    }
+    
+    // If kangaroos will meet, the relative distance between them must be a multiple of the relative speed.
+    // Moreover, x2 must be in front of x1 for them to meet, hence (x2 - x1) / (v1 - v2) should be non-negative.
+    if ((x2 - x1) % (v1 - v2) === 0 && (x2 - x1) / (v1 - v2) >= 0) {
+        return "YES";
+    }
+    
+    return "NO";
+
+}
+
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const firstMultipleInput = readLine().replace(/\s+$/g, '').split(' ');
+
+    const x1 = parseInt(firstMultipleInput[0], 10);
+
+    const v1 = parseInt(firstMultipleInput[1], 10);
+
+    const x2 = parseInt(firstMultipleInput[2], 10);
+
+    const v2 = parseInt(firstMultipleInput[3], 10);
+
+    const result = kangaroo(x1, v1, x2, v2);
+
+    ws.write(result + '\n');
+
+    ws.end();
+}
---
+++ Number Line Jumps/Python 3/main.py
@@ -0,0 +1,48 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'kangaroo' function below.
+#
+# The function is expected to return a STRING.
+# The function accepts following parameters:
+#  1. INTEGER x1
+#  2. INTEGER v1
+#  3. INTEGER x2
+#  4. INTEGER v2
+#
+
+def kangaroo(x1, v1, x2, v2):
+    if v1 <= v2 and x1 < x2 or v2 <= v1 and x2 < x1:
+        return "NO"
+    # To avoid division by zero, check if velocities are the same but starting points are different.
+    if v1 == v2 and x1 != x2:
+        return "NO"
+    # Check if the difference in distances is divisible by the difference in velocities.
+    if (x2 - x1) % (v1 - v2) == 0:
+        return "YES"
+    return "NO"
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    first_multiple_input = input().rstrip().split()
+
+    x1 = int(first_multiple_input[0])
+
+    v1 = int(first_multiple_input[1])
+
+    x2 = int(first_multiple_input[2])
+
+    v2 = int(first_multiple_input[3])
+
+    result = kangaroo(x1, v1, x2, v2)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
